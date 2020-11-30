bf064e4aaa097f3d9f19b323681a6ca877856d1b
Harry <1211009501@qq.com>
2020-11-30 21:43:29 +0800

    Add files via upload

--- libretro/libretro_core_options.h
+++ libretro/libretro_core_options.h
@@ -61,10 +61,10 @@ extern "C" {
 struct retro_core_option_definition option_defs_us[] = {
    {
       "genesis_plus_gx_system_hw",
-      "System Hardware",
-      "Runs loaded content with a specific emulated console. 'Auto' will select the most appropriate system for the current game.",
+      "系统硬件",
+      "以指定的模拟硬件运行游戏，'自动'会根据当前游戏选择最合适的系统。",
       {
-         { "auto",                 "Auto"               },
+         { "auto",                 "自动"               },
          { "sg-1000",              "SG-1000"            },
          { "sg-1000 II",           "SG-1000 II"         },
          { "mark-III",             "Mark III"           },
@@ -78,10 +78,11 @@ struct retro_core_option_definition option_defs_us[] = {
    },
    {
       "genesis_plus_gx_region_detect",
-      "System Region",
-      "Specify which region the system is from. For consoles other than the Game Gear, 'PAL' is 50hz while 'NTSC' is 60hz. Games may run faster or slower than normal if the incorrect region is selected.",
+      "系统制式",
+      "指定系统区域制式，除Game Gear外。'PAL'是50hz而'NTSC'是60hz。\n"
+      "如果选择了不正确的制式，游戏可能比正常运行速度偏快或者偏慢。",
       {
-         { "auto",    "Auto"   },
+         { "auto",    "自动"   },
          { "ntsc-u",  "NTSC-U" },
          { "pal",     "PAL"    },
          { "ntsc-j",  "NTSC-J" },
@@ -91,54 +92,60 @@ struct retro_core_option_definition option_defs_us[] = {
    },
    {
       "genesis_plus_gx_force_dtack",
-      "System Lock-Ups",
-      "Emulate system lock-ups that occur on real hardware when performing illegal address access. This should only be disabled when playing certain demos and homebrew that rely on illegal behaviour for correct operation.",
+      "系统锁定",
+      "模拟真实硬件上执行非法地址访问时发生的系统锁定。\n"
+      "此设置应该只在运行特定演示或者自制游戏时关闭，这些游戏可能依赖非法指令来完成操作。",
       {
-         { "enabled",  NULL },
-         { "disabled", NULL },
+         { "enabled",  "启用" },
+         { "disabled", "禁用" },
          { NULL, NULL },
       },
       "enabled"
    },
    {
       "genesis_plus_gx_bios",
-      "System Boot ROM",
-      "Use official BIOS/bootloader for emulated hardware, if present in RetroArch's system directory. Displays console-specific start-up sequence/animation, then runs loaded content.",
+      "系统启动ROM",
+      "使用官方BIOS/引导程序，如果在RetroArch系统目录中存在的话。\n"
+      "显示机种特定启动动画，然后运行载入的游戏。",
       {
-         { "disabled", NULL },
-         { "enabled",  NULL },
+         { "disabled", "禁用" },
+         { "enabled",  "启用" },
          { NULL, NULL },
       },
       "disabled"
    },
    {
       "genesis_plus_gx_bram",
-      "CD System BRAM",
-      "When running Sega CD content, specifies whether to share a single save file between all games from a specific region (Per-BIOS) or to create a separate save file for each game (Per-Game). Note that the Sega CD has limited internal storage, sufficient only for a handful of titles. To avoid running out of space, the 'Per-Game' setting is recommended.",
+      "CD系统BRAM",
+      "运行Sega CD游戏时，指定是在每个区域的游戏（按区域）中共享存档文件，还是为每个游戏（按游戏）单独保存存档。 \n"
+      "注意：Sega CD内部存储容量有限，只够保存几个游戏。为了避免存储空间不够用，建议选择‘按游戏’。",
       {
-         { "per bios", "Per-BIOS" },
-         { "per game", "Per-Game" },
+         { "per bios", "按区域" },
+         { "per game", "按游戏" },
          { NULL, NULL },
       },
       "per bios"
    },
    {
       "genesis_plus_gx_addr_error",
-      "68K Address Error",
-      "The Genesis CPU (Motorola 68000) produces an Address Error (crash) when attempting to perform unaligned memory access. Enabling '68K Address Error' simulates this behaviour. It should only be disabled when playing ROM hacks, since these are typically developed using less accurate emulators and may rely on invalid RAM access for correct operation.",
+      "68K寻址错误",
+      "MD的CPU（摩托罗拉68000）在执行未对齐内存访问时会产生一个寻址错误（宕机）。\n"
+      "启用此设置模拟该行为。此设置应该只在运行修改版ROM时关闭，\n"
+      "因为这些ROM通常是在不精确的模拟器上修改的，可能依赖于无效的内存访问才能正确运行。",
       {
-         { "enabled",  NULL },
-         { "disabled", NULL },
+         { "enabled",  "启用" },
+         { "disabled", "禁用" },
          { NULL, NULL },
       },
       "enabled"
    },
    {
       "genesis_plus_gx_lock_on",
-      "Cartridge Lock-On",
-      "Lock-On Technology is a Genesis feature that allowed an older game to connect to the pass-through port of a special cartridge for extended or altered gameplay. This option specifies which type of special 'lock-on' cartridge to emulate. A corresponding bios file must be present in RetroArch's system directory.",
+      "卡带锁定",
+      "锁定技术是MD的一项功能，该功能允许老游戏连接到特殊卡带的传输端口上，进行扩展游戏或者实现附加功能。\n"
+      "此设置指定模拟何种类型的‘锁定’功能。RetroArch系统目录中必须有相对应的BIOS文件。",
       {
-         { "disabled",            NULL },
+         { "disabled",            "禁用" },
          { "game genie",          "Game Genie" },
          { "action replay (pro)", "Action Replay (Pro)" },
          { "sonic & knuckles",    "Sonic & Knuckles" },
@@ -148,23 +155,27 @@ struct retro_core_option_definition option_defs_us[] = {
    },
    {
       "genesis_plus_gx_ym2413",
-      "Master System FM (YM2413)",
-      "Enable emulation of the FM Sound Unit used by certain Sega Mark III/Master System games for enhanced audio output.",
+      "Master System FM音源（YM2413）",
+      "启用Sega Mark III/Master System FM声音单元的模拟，以增强某些游戏的声音输出。",
       {
-         { "auto",     "Auto" },
-         { "disabled", NULL },
-         { "enabled",  NULL },
+         { "auto",     "自动" },
+         { "disabled", "禁用" },
+         { "enabled",  "启用" },
          { NULL, NULL },
       },
       "auto"
    },
    {
       "genesis_plus_gx_ym2612",
-      "Mega Drive / Genesis FM",
+      "Mega Drive / Genesis FM音源",
 #ifdef HAVE_YM3438_CORE
-      "Select method used to emulate the FM synthesizer (main sound generator) of the Mega Drive/Genesis. 'MAME' options are fast, and run full speed on most systems. 'Nuked' options are cycle accurate, very high quality, and have substantial CPU requirements. The 'YM2612' chip is used by the original Model 1 Genesis. The 'YM3438' is used in later Genesis revisions.",
+      "选择模拟MD/Genesis FM合成器（主声音发生器）的方式。\n"
+      "'MAME'方式较快，在绝大多数系统上都可以全速运行。\n"
+      "'Nuked'方式较精确，声音质量更高，但是需要更高的CPU性能。\n"
+      "'YM2612'芯片由早期MD1型机使用，'YM3438'由后期MD机型使用。",
 #else
-      "Select method used to emulate the FM synthesizer (main sound generator) of the Mega Drive/Genesis. The 'YM2612' chip is used by the original Model 1 Genesis. The 'YM3438' is used in later Genesis revisions.",
+      "选择模拟MD/Genesis FM合成器（主声音发生器）的方式。\n"
+      "'YM2612'芯片由早期MD1型机使用，'YM3438'由后期MD机型使用。",
 #endif
       {
          { "mame (ym2612)",          "MAME (YM2612)" },
@@ -180,19 +191,20 @@ struct retro_core_option_definition option_defs_us[] = {
    },
    {
       "genesis_plus_gx_sound_output",
-      "Sound Output",
-      "Select stereo or mono sound reproduction.",
+      "输出声道",
+      "选择立体声或单声道声音输出。",
       {
-         { "stereo", "Stereo" },
-         { "mono",   "Mono" },
+         { "stereo", "立体声" },
+         { "mono",   "单声道" },
          { NULL, NULL },
       },
       "stereo"
    },
    {
       "genesis_plus_gx_psg_preamp",
-      "PSG Preamp Level",
-      "Set the audio preamplifier level of the emulated SN76496 4-channel Programmable Sound Generator found in the Master System, Game Gear and Genesis.",
+      "PSG预放大级别",
+      "设置的SN76496四通道可编程声音发生器的声音预放大级别，\n"
+      "此芯片用于Master System、Game Gear和MD/Genesis。",
       {
          { "0",   NULL },
          { "5",   NULL },
@@ -241,8 +253,8 @@ struct retro_core_option_definition option_defs_us[] = {
    },
    {
       "genesis_plus_gx_fm_preamp",
-      "FM Preamp Level",
-      "Set the audio preamplifier level of the emulated Sega Mark III/Master System FM Sound Unit.",
+      "FM预放大级别",
+      "设置FM音频芯片的预放大级别，此芯片用于Sega Mark III/Master System。",
       {
          { "0",   NULL },
          { "5",   NULL },
@@ -291,19 +303,20 @@ struct retro_core_option_definition option_defs_us[] = {
    },
    {
       "genesis_plus_gx_audio_filter",
-      "Audio Filter",
-      "Enable a low pass audio filter to better simulate the characteristic sound of a Model 1 Genesis.",
+      "音频过滤",
+      "启用音频低通滤镜，以更好地模拟MD1型机的声音特性。",
       {
-         { "disabled", NULL },
-         { "low-pass", "Low-Pass" },
+         { "disabled", "禁用" },
+         { "low-pass", "低通滤镜" },
          { NULL, NULL },
       },
       "disabled"
    },
    {
       "genesis_plus_gx_lowpass_range",
-      "Low-Pass Filter %",
-      "Specify the cut-off frequency of the audio low pass filter. A higher value increases the perceived 'strength' of the filter, since a wider range of the high frequency spectrum is attenuated.",
+      "低通滤镜强度%",
+      "设置低通滤镜的截断频率。\n"
+      "值越高，可察觉到的过滤强度越高，因为更宽范围的高频扩展被减弱了。",
       {
          { "5",  NULL },
          { "10", NULL },
@@ -331,8 +344,8 @@ struct retro_core_option_definition option_defs_us[] = {
 #ifdef HAVE_EQ
    {
       "genesis_plus_gx_audio_eq_low",
-      "EQ Low",
-      "Adjust the low range band of the internal audio equaliser.",
+      "EQ低频",
+      "调整内置音频均衡器的低音频带。",
       {
          { "0",   NULL },
          { "5",   NULL },
@@ -361,8 +374,8 @@ struct retro_core_option_definition option_defs_us[] = {
    },
    {
       "genesis_plus_gx_audio_eq_mid",
-      "EQ Mid",
-      "Adjust the middle range band of the internal audio equaliser.",
+      "EQ中频",
+      "调整内置音频均衡器的中音频带。",
       {
          { "0",   NULL },
          { "5",   NULL },
@@ -391,8 +404,8 @@ struct retro_core_option_definition option_defs_us[] = {
    },
    {
       "genesis_plus_gx_audio_eq_high",
-      "EQ High",
-      "Adjust the high range band of the internal audio equaliser.",
+      "EQ高频",
+      "调整内置音频均衡器的高音频带。",
       {
          { "0",   NULL },
          { "5",   NULL },
@@ -422,20 +435,20 @@ struct retro_core_option_definition option_defs_us[] = {
 #endif
    {
       "genesis_plus_gx_frameskip",
-      "Frameskip",
-      "Skip frames to avoid audio buffer under-run (crackling). Improves performance at the expense of visual smoothness. 'Auto' skips frames when advised by the frontend. 'Manual' utilises the 'Frameskip Threshold (%)' setting.",
+      "跳帧",
+      "跳帧避免破音. 改善表现牺牲流畅度. '自动' 根据前台建议跳帧. '手动' 利用跳帧阈值 (%)'设定跳帧.",
       {
-         { "disabled", NULL },
-         { "auto",     "Auto" },
-         { "manual",   "Manual" },
+         { "disabled", "禁用" },
+         { "auto",     "自动" },
+         { "manual",   "手动" },
          { NULL, NULL },
       },
       "disabled"
    },
    {
       "genesis_plus_gx_frameskip_threshold",
-      "Frameskip Threshold (%)",
-      "When 'Frameskip' is set to 'Manual', specifies the audio buffer occupancy threshold (percentage) below which frames will be skipped. Higher values reduce the risk of crackling by causing frames to be dropped more frequently.",
+      "跳帧阈值 (%)",
+      " '跳帧'设定成'手动'时, 指定的音频缓冲占比低于阈值时跳帧. 值越高丢帧越多, 但破音几率更低.",
       {
          { "15", NULL },
          { "18", NULL },
@@ -459,12 +472,12 @@ struct retro_core_option_definition option_defs_us[] = {
    },
    {
       "genesis_plus_gx_blargg_ntsc_filter",
-      "Blargg NTSC Filter",
-      "Apply a video filter to mimic various NTSC TV signals.",
+      "Blargg NTSC滤镜",
+      "应用一个视频滤镜来模拟不同的NTSC电视信号。",
       {
-         { "disabled",   NULL },
-         { "monochrome", "Monochrome" },
-         { "composite",  "Composite" },
+         { "disabled",   "禁用" },
+         { "monochrome", "单色" },
+         { "composite",  "复合视频" },
          { "svideo",     "S-Video" },
          { "rgb",        "RGB" },
          { NULL, NULL },
@@ -473,54 +486,60 @@ struct retro_core_option_definition option_defs_us[] = {
    },
    {
       "genesis_plus_gx_lcd_filter",
-      "LCD Ghosting Filter",
-      "Apply an image 'ghosting' filter to mimic the display characteristics of the Game Gear and 'Genesis Nomad' LCD panels.",
+      "LCD鬼影滤镜",
+      "应用一个图像鬼影滤镜，来模拟Game Gear和Genesis Nomad的LCD屏幕显示特性。",
       {
-         { "disabled", NULL },
-         { "enabled",  NULL },
+         { "disabled", "禁用" },
+         { "enabled",  "启用" },
          { NULL, NULL },
       },
       "disabled"
    },
    {
       "genesis_plus_gx_overscan",
-      "Borders",
-      "Enable this to display the overscan regions at the top/bottom and/or left/right of the screen. These would normally be hidden by the bezel around the edge of a standard-definition television.",
+      "边界",
+      "启用此设置显示屏幕上下或者左右的过扫描区域。\n"
+      "这些区域通常被标准电视的边框隐藏了。",
       {
-         { "disabled",   NULL },
-         { "top/bottom", "Top/Bottom" },
-         { "left/right", "Left/Right" },
-         { "full",       "Full" },
+         { "disabled",   "禁用" },
+         { "top/bottom", "上/下" },
+         { "left/right", "左/右" },
+         { "full",       "全部" },
          { NULL, NULL },
       },
       "disabled"
    },
    {
       "genesis_plus_gx_gg_extra",
-      "Game Gear Extended Screen",
-      "Forces Game Gear titles to run in 'SMS' mode, with an increased resolution of 256x192. May show additional content, but typically displays a border of corrupt/unwanted image data.",
+      "扩展Game Gear屏幕",
+      "强制Game Gear游戏以SMS模式运行，提高分辨率到256x192。\n"
+      "可能会显示额外的内容，但是通常是显示一个含有无效图像数据的边框。",
       {
-         { "disabled", NULL },
-         { "enabled",  NULL },
+         { "disabled", "禁用" },
+         { "enabled",  "启用" },
          { NULL, NULL },
       },
       "disabled"
    },
    {
       "genesis_plus_gx_aspect_ratio",
-      "Core-Provided Aspect Ratio",
-      "Choose the preferred content aspect ratio. This will only apply when RetroArch's aspect ratio is set to 'Core provided' in the Video settings.",
+      "内核提供宽高比",
+      "选择期望的显示宽高比。\n"
+      "此项设置只有当RetroArch的宽高比设为‘内核提供’时有效。",
       {
-         { "auto",     "Auto" },
-         { "NTSC PAR", NULL },
-         { "PAL PAR",  NULL },
+         { "auto",     "自动" },
+         { "NTSC PAR", "NTSC像素宽高比" },
+         { "PAL PAR",  "PAL像素宽高比" },
       },
       "auto"
    },
    {
       "genesis_plus_gx_render",
-      "Interlaced Mode 2 Output",
-      "Interlaced Mode 2 allows the Genesis to output a double height (high resolution) 320x448 image by drawing alternate scanlines each frame (this is used by 'Sonic the Hedgehog 2' and 'Combat Cars' multiplayer modes). 'Double Field' mimics original hardware, producing a sharp image with flickering/interlacing artefacts. 'Single Field' apples a de-interlacing filter, which stabilises the image but causes mild blurring.",
+      "隔行模式2输出",
+      "隔行模式2允许MD/Genesis在每帧绘制额外的扫描线，输出双倍高度的图像（320x448）\n"
+      "此功能由'Sonic the Hedgehog 2'和'Combat Cars'多人游戏模式使用。\n"
+      "'双场'模拟原始硬件，输出更锐利的图像同时伴随抖动/隔行扫描效果。\n"
+      "'单场'应用一个去隔行滤镜，产生稳定的图像但是可能导致轻微模糊。",
       {
          { "single field", "Single Field" },
          { "double field", "Double Field" },
@@ -530,33 +549,33 @@ struct retro_core_option_definition option_defs_us[] = {
    },
    {
       "genesis_plus_gx_gun_cursor",
-      "Show Light Gun Crosshair",
-      "Display light gun crosshairs when using the 'MD Menacer', 'MD Justifiers' and 'MS Light Phaser' input device types.",
+      "显示光枪准心",
+      "在使用'MD Menacer', 'MD Justifiers'和'MS Light Phaser'输入设备时显示光枪准心。",
       {
-         { "disabled", NULL },
-         { "enabled",  NULL },
+         { "disabled", "禁用" },
+         { "enabled",  "启用" },
          { NULL, NULL },
       },
       "disabled"
    },
    {
       "genesis_plus_gx_gun_input",
-      "Light Gun Input",
-      "Use a mouse-controlled 'Light Gun' or 'Touchscreen' input.",
+      "光枪输入",
+      "使用鼠标控制的‘光枪’或者‘触屏’输入。",
       {
-         { "lightgun",    "Light Gun" },
-         { "touchscreen", "Touchscreen" },
+         { "lightgun",    "光枪" },
+         { "touchscreen", "触屏" },
          { NULL, NULL },
       },
       "lightgun"
    },
    {
       "genesis_plus_gx_invert_mouse",
-      "Invert Mouse Y-Axis",
-      "Inverts the Y-axis of the 'MD Mouse' input device type.",
+      "鼠标Y轴反向",
+      "使用'MD鼠标'输入设备时反向Y轴。",
       {
-         { "disabled", NULL },
-         { "enabled",  NULL },
+         { "disabled", "禁用" },
+         { "enabled",  "启用" },
          { NULL, NULL },
       },
       "disabled"
@@ -564,8 +583,8 @@ struct retro_core_option_definition option_defs_us[] = {
 #ifdef HAVE_OVERCLOCK
    {
       "genesis_plus_gx_overclock",
-      "CPU Speed",
-      "Overclock the emulated CPU. Can reduce slowdown, but may cause glitches.",
+      "CPU速度",
+      "对模拟CPU进行超频。可以减少拖慢，但可能导致其他问题。",
       {
          { "100%", NULL },
          { "125%", NULL },
@@ -579,11 +598,12 @@ struct retro_core_option_definition option_defs_us[] = {
 #endif
    {
       "genesis_plus_gx_no_sprite_limit",
-      "Remove Per-Line Sprite Limit",
-      "Removes the 8 (Master System) or 20 (Genesis) sprite-per-scanline hardware limit. This reduces flickering but can cause visual glitches, as some games exploit the hardware limit to generate special effects.",
+      "移除每扫描线活动块数量限制",
+      "移除每扫描线的8个（Master System）或20个（MD/Genesis）活动块数量硬件限制。\n"
+      "可以减少闪烁，但是可能导致视觉效果不正常，因为有些游戏使用此限制来产生特殊效果。",
       {
-         { "disabled", NULL },
-         { "enabled",  NULL },
+         { "disabled", "禁用" },
+         { "enabled",  "启用" },
          { NULL, NULL },
       },
       "disabled"
@@ -611,7 +631,7 @@ struct retro_core_option_definition *option_defs_intl[RETRO_LANGUAGE_LAST] = {
    NULL,           /* RETRO_LANGUAGE_RUSSIAN */
    NULL,           /* RETRO_LANGUAGE_KOREAN */
    NULL,           /* RETRO_LANGUAGE_CHINESE_TRADITIONAL */
-   option_defs_chs,/* RETRO_LANGUAGE_CHINESE_SIMPLIFIED */
+   NULL,           /* RETRO_LANGUAGE_CHINESE_SIMPLIFIED */
    NULL,           /* RETRO_LANGUAGE_ESPERANTO */
    NULL,           /* RETRO_LANGUAGE_POLISH */
    NULL,           /* RETRO_LANGUAGE_VIETNAMESE */
